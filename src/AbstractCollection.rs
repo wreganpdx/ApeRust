af5dc30f9240385beb60701584390ad6e03a3e0e
willotheblessed <regan.bill@gmail.com>
2019-04-30 11:40:47 -0700

    Initial collection of classes
    Initial collection of classes

---
+++ src/AbstractCollection.rs
@@ -0,0 +1,14 @@
+/*
+William Regan
+Tyler Pelham
+Portland State University
+Rust Programming 510 
+Final Project
+*/
+
+/**
+ * AbstractCollection.rs
+ * 
+ * Summary: Should allow grouping of objects for collision detection
+ * ore information, see  https://github.com/arctwelve/ape-js-port/tree/master/org/cove/ape
+ */
---
+++ src/Group.rs
@@ -0,0 +1,16 @@
+/*
+William Regan
+Tyler Pelham
+Portland State University
+Rust Programming 510 
+Final Project
+*/
+
+/**
+ * Group.rs
+ * 
+ * Summary: Extends Abstact Collection and allows for further grouping of objects.
+ * 
+ * For more information, see  https://github.com/arctwelve/ape-js-port/tree/master/org/cove/ape
+ */
+
