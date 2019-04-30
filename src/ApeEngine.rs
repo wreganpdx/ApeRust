cdc1b228def7c9613c3c94535da7188e75f4ba32
willotheblessed <regan.bill@gmail.com>
2019-04-30 11:31:58 -0700

    First ApeEngine.rs commit
    ApeEngine.rs commit

---
+++ src/ApeEngine.rs
@@ -0,0 +1,17 @@
+/*
+William Regan
+Tyler Pelham
+Portland State University
+Rust Programming 510 
+Final Project
+*/
+
+/**
+ * ApeEngine.rs
+ * 
+ * Summary: This is the core of the Ape Engine
+ * Functions to impliment include, adding objects, stepping through physics simulations
+ * and painting.
+ * ore information, see https://exercism.io/my/tracks/rust
+ */
+
