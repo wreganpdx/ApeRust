1d4c33568782eec6c86ff4e5a38b27fc9057c647
willotheblessed <regan.bill@gmail.com>
2019-04-30 17:56:38 -0700

    added some more functions for vec
    v5 = v5.normalize();

--- src/demos/src/main.rs
+++ src/demos/src/main.rs
@@ -4,5 +4,23 @@ use ApeRust::vector::vector;
 fn main() 
 {
     let mut v:vector = vector::new(2.0,4.0);
-    println!("Hello, World! {:?}", v);
+    let mut v2:vector = vector::new(0.0,0.0);
+    let mut v3:vector = vector::new(0.0,0.0);
+    let mut v4:vector = vector::new(0.0,0.0);
+    let mut v5:vector = vector::new(0.0,0.0);
+    v2.copy(&v);
+    v2.plusEquals(&v);
+    v3.copy(&v2);
+    v4.copy(&v2);
+    v2.minusEquals(&v);
+    v4 = v4.mult(5.0);
+    v4 = v4.mult(6.0);
+    v5 = v4.times(&v4);
+    v5.divEquals(3.0);
+    let c = v5.magnitude();
+    let t = v2.distance(&v3);
+    v5 = v5.normalize();
+    v5 = v5.rotate(&0.52);
+    println!("Hello, World! {:?}, {:?}, {}, {}, {:?}, {:?}, {:?}, {:?}", c, t, v2.cross(&v), v2.dot(&v), v2.plus(&v), v2.minus(&v), v3, v5);
+
 }
--- src/pdx/ape/vector.rs
+++ src/pdx/ape/vector.rs
@@ -25,9 +25,134 @@ pub struct vector
 
 impl vector
 {
-	pub fn new(x: f64, y: f64) -> vector {
+	pub fn new(x: f64, y: f64) -> vector 
+    {
         vector { x: x, y: y }
     }
+
+    pub fn set_to(mut self:vector, px: f64, py: f64)
+    {
+
+		self.x = px;
+		self.y = py;
+	}
+    pub fn copy(&mut self, v:&vector)
+    {
+        self.x = v.x;
+        self.y = v.y;
+	}
+
+    pub fn dot(&self, v:&vector)->f64 
+    {
+		return self.x * v.x + self.y * v.y;
+	}
+
+    pub fn cross(&self, v:&vector)-> f64
+    {
+		return self.x * v.y - self.y * v.x;
+	}
+
+    pub fn plus(&self, v:&vector)-> vector
+    {
+		return vector::new(self.x + v.x, self.y + v.y);
+	}
+
+    		
+	pub fn plusEquals(&mut self, v:&vector)->&vector 
+    {
+        self.x += v.x;
+        self.y += v.y;
+        return self as &vector;
+	}
+
+    pub fn minus(&self, v:&vector) ->vector 
+    {
+		return vector::new(self.x - v.x, self.y - v.y);  
+	}
+
+    pub fn minusEquals(&mut self, v:&vector)->&vector 
+    {
+        self.x -= v.x;
+        self.y -= v.y;
+        return self as &vector;
+	}
+
+    pub fn mult(& self, s:f64)->vector 
+    {
+        return vector::new(self.x *s, self.y *s);
+	}
+
+    pub fn multEquals(&mut self, s:f64)->&vector 
+    {
+        self.x *= s;
+        self.y *= s;
+        return self as &vector;
+	}
+
+    pub fn times(&self, v:&vector) ->vector 
+    {
+		return vector::new(self.x * v.x, self.y * v.y);  
+	}
+
+    pub fn divEquals(&mut self, mut s:f64)->&vector 
+    {
+        if s == 0.0
+        { s = 0.0001;}
+        self.x /= s;
+        self.y /= s;
+        return self as &vector;
+	}
+
+    pub fn magnitude(&self)->f64 
+    {
+        let w:f64 = self.x * self.x + self.y * self.y;
+        if w == 0.0
+        {
+            return 0.0;
+        }
+        let mut b:f64 = w * 0.25;
+        let mut c:f64 = w/b;
+        b = (b+c) * 0.5;
+        let mut a:f64 = b-c;
+        if (a < 0.0)
+        {
+            a *= -1.0;
+        }
+        while (a > 0.2)
+        {
+            c = w/b;
+            b = (b+c) * 0.5;
+            a = b - c;
+        }
+
+		return b;
+	}
+
+    pub fn distance(&self, v:&vector)->f64 
+    {
+        let delta:vector = self.minus(&v);
+        let mut mag:f64 = delta.magnitude();
+        if (mag == 0.0)
+        {
+            mag = 0.0001;
+        }
+        return mag;
+    }
+    pub fn normalize(&mut self)->vector 
+    {
+        let mut m:f64 = self.magnitude();
+        if m == 0.0
+        {
+            m = 0.0001;
+        }
+        return self.mult(1.0/m);
+	}
+	pub fn rotate(&self, r:&f64) ->vector 
+    {
+        let c = r.cos();
+        let s = r.sin();
+        return vector::new(self.x*c-self.y*s,self.x*s+self.y*c);
+	}
 }
 
 impl fmt::Debug for vector {
@@ -39,120 +164,7 @@ impl fmt::Debug for vector {
 /*
 	import flash.filters.ColorMatrixFilter;
 	import flash.geom.Matrix;
-	
-	public class Vector {
-		
-		public var x:Number;
-		public var y:Number;
-	
-	
-		public function Vector(px:Number = 0, py:Number = 0) {
-			x = px;
-			y = py;
-		}
-		
-		
-		public function setTo(px:Number, py:Number):void {
-			x = px;
-			y = py;
-		}
-		
-		
-		public function copy(v:Vector):void {
-			x = v.x;
-			y = v.y;
-		}
-	
-	
-		public function dot(v:Vector):Number {
-			return x * v.x + y * v.y;
-		}
-		
-		
-		public function cross(v:Vector):Number {
-			return x * v.y - y * v.x;
-		}
-		
-	
-		public function plus(v:Vector):Vector {
-			return new Vector(x + v.x, y + v.y); 
-		}
-	
-		
-		public function plusEquals(v:Vector):Vector {
-			x += v.x;
-			y += v.y;
-			return this;
-		}
-		
-		
-		public function minus(v:Vector):Vector {
-			return new Vector(x - v.x, y - v.y);    
-		}
-	
-	
-		public function minusEquals(v:Vector):Vector {
-			x -= v.x;
-			y -= v.y;
-			return this;
-		}
-	
-	
-		public function mult(s:Number):Vector {
-			return new Vector(x * s, y * s);
-		}
-	
-	
-		public function multEquals(s:Number):Vector {
-			x *= s;
-			y *= s;
-			return this;
-		}
-	
-	
-		public function times(v:Vector):Vector {
-			return new Vector(x * v.x, y * v.y);
-		}
-		
-		
-		public function divEquals(s:Number):Vector {
-			if (s == 0) s = 0.0001;
-			x /= s;
-			y /= s;
-			return this;
-		}
-		
-		
-		public function magnitude():Number {
-			//return Math.sqrt(x * x + y * y);
-			var w:Number = (x * x + y * y);
-			if(w == 0) return 0;
-			var b:Number = w * 0.25;
-			var c:Number = 0;
-			var a:Number = 0;
-			do {
-				c = w / b;
-				b = (b + c) * 0.5;
-				a = b - c;
-				if (a < 0)  a = -a;
-			} while (a > .2);
-			return b;
-		}
 
-		
-		public function distance(v:Vector):Number {
-			var delta:Vector = this.minus(v);
-			var mag:Number = delta.magnitude();
-			if (mag == 0) mag = 0.0001;
-			return mag;
-		}
-
-	
-		public function normalize():Vector {
-			 var m:Number = magnitude();
-			 if (m == 0) m = 0.0001;
-			 return mult(1 / m);
-		}
 		
 				
 		public function toString():String {
@@ -178,12 +190,7 @@ impl fmt::Debug for vector {
 			y = x * m.c + y * m.d;
 			return this;
 		}
-		
-		public function rotate(r:Number):Vector{
-			var c=Math.cos(r);
-			var s=Math.sin(r);
-			return new Vector(x*c-y*s,x*s+y*c);
-		}
+
 	}
 }
 */
